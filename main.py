a77e4034539ba4e0d9f79c69ecd71c78eebbe1f6
denys.vakhovych@ihsmarkit.com <Denys.Vakhovych@ihsmarkit.com>
2022-03-01 09:51:13 +0200

    rough draft

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-import asyncio
 import logging
-import os
-import time
-from pyrogram import Client, filters
-from pyrogram.errors import FloodWait
-
-from pyrogram.types import ChatPermissions
-
-import time
-from time import sleep
-import random
+import json
+from urllib.request import urlopen
 from pyrogram import Client
 from pyrogram import filters
-from pytgcalls import idle
-from pytgcalls import PyTgCalls
-from pytgcalls import StreamType
-from pytgcalls.types.input_stream import InputAudioStream
-from pytgcalls.types.input_stream import InputStream
-
-import misc
-
-POOL_SIZE = 10
 
 app = Client("guardian")
-app_caller = PyTgCalls(app)
-app_caller.start()
-
 father = "meaffs"
 alarm_msg_markers = ['укритт',
                      'сирен',
@@ -40,75 +19,6 @@ telebot_uri = 'http://api.callmebot.com/start.php?' \
               '&rpt=2' \
               '&cc=yes'
 # '&timeout=[time out]'
-
-
-def make_client():
-    return Client("caller")
-
-
-async def make_call(username):
-    file = './input.raw'
-    while not os.path.exists(file):
-        time.sleep(0.125)
-    await get_caller().join_group_call(
-        username,
-        InputStream(
-            InputAudioStream(
-                file,
-            ),
-        ),
-        stream_type=StreamType().local_stream,
-    )
-
-
-async def hang(username):
-    await get_caller().leave_group_call(
-        username,
-    )
-
-
-@app.on_message(filters.regex('!p1'))
-async def call2(_, msg):
-    file = './input.raw'
-    while not os.path.exists(file):
-        await asyncio.sleep(0.125)
-    await app_caller.join_group_call(
-        msg.chat.id,
-        InputStream(
-            InputAudioStream(
-                file,
-            ),
-        ),
-        stream_type=StreamType().local_stream,
-    )
-
-
-@app.on_message(filters.regex('!s1'))
-async def hang2(_, msg):
-    await app_caller.leave_group_call(
-        msg.chat.id,
-    )
-
-
-def get_caller():
-    return app_caller
-    # last_active[0] += 1
-    #
-    # return hotline[last_active[0] % 10]
-
-
-# clients_pool = [make_client()] * POOL_SIZE
-# hotline = []
-# for cli in clients_pool:
-#     caller = PyTgCalls(cli)
-#     caller.start()
-#     hotline.append(PyTgCalls(cli))
-#
-# last_active = [0]
-
-###############################################################################
-################################  CALL PART END
-###############################################################################
 
 @app.on_message(filters.text & filters.user)
 def join_channel_request(_, msg):
@@ -146,45 +56,8 @@ def join_channel(username, channel):
 def alert_user(username):
     logging.log(logging.INFO, username + 'alerted')
     app.send_message(username, "test alert message")
-    asyncio.get_event_loop().run_until_complete(make_call(username))
+    url = telebot_uri.format('@' + username, 'test+message')
+    with urlopen(url) as f:
+        print(json.load(f))
 
-
-    # url = telebot_uri.format('@' + username, )
-    # with urlopen(url) as f:
-    #     resp = json.load(f)
-
-
-
-
-####################################################################
-####################################################################
-####################################################################
-####################################################################
-
-# Команда type
-@app.on_message(filters.command("type", prefixes=".") & filters.me)
-def type(_, msg):
-    orig_text = msg.text.split(".type ", maxsplit=1)[1]
-    text = orig_text
-    tbp = "" # to be printed
-    typing_symbol = "▒"
-
-    while(tbp != orig_text):
-        try:
-            msg.edit(tbp + typing_symbol)
-            sleep(0.05) # 50 ms
-
-            tbp = tbp + text[0]
-            text = text[1:]
-
-            msg.edit(tbp)
-            sleep(0.05)
-
-        except FloodWait as e:
-            sleep(e.x)
-
-
-
-# misc.use_all()
-# app.run()
-idle()
+app.run()
