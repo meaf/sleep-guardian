8f0dc8741ec55905c752b680629530fa607ddec3
denys.vakhovych@ihsmarkit.com <Denys.Vakhovych@ihsmarkit.com>
2022-03-01 23:53:38 +0200

    added more commands, stabilised existing method calls

---
+++ constants.py
@@ -0,0 +1,17 @@
+father = "meaffs"
+
+HELP_MSG1 = "Hi! I will help you keep track of all important alarms going off " \
+            "here and there, and fuck putin btw"
+HELP_MSG2 = "It utilizes external tool (callmebot) and you will have to " \
+            "register yourself here: https://api2.callmebot.com/txt/login.php"
+HELP_MSG3 = "the list of available commands:\n" \
+            "/join @channel - join a channel, you will receive a call from " \
+            "bot, you will need to register it\n" \
+            "/leave @channel - unsubscribes you from a channel alerts\n" \
+            "/test_call - make a test call, I recommend you doing this after " \
+            "registration and add bot to contacts\n" \
+            "/ping - ping if bot is running (should always be and ping back, " \
+            "but just in case...)\n"\
+            "/help - well.. this"
+HELP_MSG4 = "list of word parts that alarm goes off to: {}"
+
--- main.py
+++ main.py
@@ -1,32 +1,53 @@
-import logging
 import json
+import logging
 from urllib.request import urlopen
-from pyrogram import Client
+
+from pyrogram import Client, idle
 from pyrogram import filters
+from pyrogram.errors import UsernameInvalid
+
+from constants import *
 
 app = Client("guardian")
-father = "meaffs"
-alarm_msg_markers = ['укритт',
-                     'сирен',
-                     'тривог'
-                     ]
 
-track_channels = {'yaremche_live': [father], 'boryspilnews': [father]}
 
-telebot_uri = 'http://api.callmebot.com/start.php?' \
-              'user={}' \
-              '&text={}' \
-              '&rpt=2' \
-              '&cc=yes'
-# '&timeout=[time out]'
-
-@app.on_message(filters.text & filters.user)
+@app.on_message(filters.command("join", prefixes="/"))
 def join_channel_request(_, msg):
-    orig_text = msg.text
-    app.send_message(father, "request from " + msg.from_user.username
-                     + " to join channel: ")
-    app.send_message(father, orig_text)
-    app.join_chat(orig_text)
+    try:
+        channel_name = msg.text.split("/join ", maxsplit=1)[1]
+        app.send_message(father, "request from " +
+                         msg.from_user.username
+                         + " to join channel " + channel_name)
+        join_channel(msg.from_user.username, channel_name)
+        app.send_message(msg.from_user.username, "Joined " + channel_name)
+    except UsernameInvalid as e:
+        app.send_message(msg.from_user.username, "Failed to join a channel! "
+                                                 "request format:"
+                                                 "\n/join @channelname")
+
+
+@app.on_message(filters.command("leave", prefixes="/"))
+def leave_channel_request(_, msg):
+    try:
+        channel_name = msg.text.split("/leave ", maxsplit=1)[1]
+        app.send_message(father, "request from " +
+                         msg.from_user.username
+                         + " to leave channel " + channel_name)
+        if leave_channel(msg.from_user.username, channel_name):
+            app.send_message(msg.from_user.username, "Joined " + channel_name)
+    except UsernameInvalid as e:
+        app.send_message(msg.from_user.username, "Failed to join a channel! "
+                                                 "request format:"
+                                                 "\n/join @channelname")
+
+
+@app.on_message(filters.command("help", prefixes="/"))
+def show_help(_, msg):
+    app.send_message(msg.from_user.username, HELP_MSG1)
+    app.send_message(msg.from_user.username, HELP_MSG2)
+    app.send_message(msg.from_user.username, HELP_MSG3)
+    app.send_message(msg.from_user.username, HELP_MSG4)
+    app.send_message(msg.from_user.username, alarm_msg_markers.__str__)
 
 
 @app.on_message(filters.channel & filters.text)
@@ -38,26 +59,80 @@ def alert(_, msg):
             if channel in track_channels:
                 users_to_alert = track_channels[channel]
                 for username in users_to_alert:
-                    alert_user(username)
-                    logging.log(logging.INFO, username + 'alerted')
+                    alert_user(username, msg)
+                    logging.log(logging.INFO, username + "alerted")
             else:
                 logging.log(logging.WARNING,
                             " channel has no recipients: " + orig_text
-                            + " markers: " + alarm_msg_markers.__str__()
-                            + " track_channels: " + track_channels.__str__())
-                alert_user(father)
+                            + "\nmarkers: " + alarm_msg_markers.__str__()
+                            + "\ntrack_channels: " + track_channels.__str__())
+                alert_user(father, msg)
+
+
+@app.on_message(filters.command("ping", prefixes="/"))
+def ping_back(_, msg):
+    app.send_message(msg.from_user.username, "I'm here =)")
+
+
+@app.on_message(filters.command("test_call", prefixes="/"))
+def ping_back(_, msg):
+    app.send_message(msg.from_user.username, "I'm here =)")
 
 
 def join_channel(username, channel):
     app.join_chat(channel)
-    track_channels[channel] += [username]
+    channel = channel.replace("@", "")
+    if channel not in track_channels:
+        track_channels[channel] = []
+    if username not in track_channels[channel]:
+        track_channels[channel].append(username)
+    save_dictionary()
+
+def leave_channel(username, channel):
+    channel = channel.replace("@", "")
+    if channel not in track_channels:
+        app.send_message(username,
+                         "you are not subscribed to this channel's alerts")
+        return False
+    if username not in track_channels[channel]:
+        app.send_message(username,
+                         "you are not subscribed to this channel's alerts")
+        return False
+    track_channels[channel].remove(username)
+    if not track_channels[channel]:
+        app.leave_chat(channel)
+    save_dictionary()
+    return True
 
 
-def alert_user(username):
-    logging.log(logging.INFO, username + 'alerted')
-    app.send_message(username, "test alert message")
-    url = telebot_uri.format('@' + username, 'test+message')
+def alert_user(username, msg):
+    app.send_message(username, msg.sender_chat.title + ": " + msg.text)
+    url = telebot_uri.format("@" + username, "Alert!")
     with urlopen(url) as f:
         print(json.load(f))
+    logging.log(logging.INFO, username + "alerted")
 
-app.run()
+
+def save_dictionary():
+    with open("./store/subscriptions", "w") as data:
+        data.write(str(track_channels))
+
+
+def load_data(path):
+    with open(path) as f:
+        data = f.read()
+    return json.loads(data.replace("'", '"'))
+
+
+def on_startup():
+    app.send_message(father, "Guardian started...")
+
+
+telebot_uri = "http://api.callmebot.com/start.php?user={}&text={}&rpt=2&cc=yes"
+alarm_msg_markers = load_data("./store/markers")
+track_channels = load_data("./store/subscriptions")
+
+app.start()
+on_startup()
+
+idle()
