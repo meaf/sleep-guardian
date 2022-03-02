51ae6bbedcc520e28640846e66d4919e9fa94895
Den <Denys.Vakhovych@ihsmarkit.com>
2022-03-02 14:52:43 +0200

    improved help menu

--- constants.py
+++ constants.py
@@ -2,8 +2,7 @@ father = "meaffs"
 
 HELP_MSG1 = "Hi! I will help you keep track of all important alarms going off " \
             "here and there, and fuck putin btw"
-HELP_MSG2 = "It utilizes external tool (callmebot) and you will have to " \
-            "register yourself here: https://api2.callmebot.com/txt/login.php"
+HELP_MSG2 = "It utilizes external tool (callmebot)"
 HELP_MSG3 = "the list of available commands:\n" \
             "/join @channel - join a channel, you will receive a call from " \
             "bot, you will need to register it\n" \
@@ -15,6 +14,9 @@ HELP_MSG3 = "the list of available commands:\n" \
             "/help - well.. this"
 HELP_MSG4 = "the list of word parts that alarm goes off to: "
 
-HELP_MSG5 = 'Please go to "Settings" -> "Privacy and Security" and make sure ' \
-            'that the "Calls" from everybody or your contacts ' \
-            '(but add @CallMeBot_API8)'
+HELP_MSG5 = '!!!IMPORTANT!!! ' \
+            '1. Please go to "Settings" -> "Privacy and Security" and make ' \
+            'sure that the "Calls" from everybody or your contacts (but add ' \
+            '@CallMeBot_API8 to contacts)\n' \
+            '2. register on https://api2.callmebot.com/txt/login.php\n' \
+            '3. after that make a /test_call to make sure everything is OK'
